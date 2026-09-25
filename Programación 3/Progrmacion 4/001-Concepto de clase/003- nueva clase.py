aff4349249616a2d9ceab468faa5732b4c11f6ce
NOFAL AHMAD RANA <nofalahmad456@gmail.com>
2026-09-25 13:31:51 +0200

    Delete Programación 3/Progrmacion 4 directory

--- Programación 3/Progrmacion 4/001-Concepto de clase/003- nueva clase.py
+++
@@ -1,4 +0,0 @@
-class Alumno():
-  def __init__(self):
-    self.nombre = ""
-    self.apellidos = ""
--- Programación 3/Progrmacion 4/004-Crecion de metodos/001-clase base.py
+++
@@ -1,8 +0,0 @@
-class Alumno():
-  def __init__(self):
-    self.nombre = ""
-    self.apellidos = ""
-    self.fecha_de_nacimiento = ""
-    self.correo = ""
-    self.telefono = ""
-  
